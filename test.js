b3acba1aa30456b8648cde9c8a52b7833469884a
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:34:56 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,21 +1,19 @@
-'use strict';
-var test = require('ava');
-var leven = require('./');
+import test from 'ava';
+import m from './';
 
-test(function (t) {
-	t.is(leven('a', 'b'), 1);
-	t.is(leven('ab', 'ac'), 1);
-	t.is(leven('ac', 'bc'), 1);
-	t.is(leven('abc', 'axc'), 1);
-	t.is(leven('kitten', 'sitting'), 3);
-	t.is(leven('xabxcdxxefxgx', '1ab2cd34ef5g6'), 6);
-	t.is(leven('cat', 'cow'), 2);
-	t.is(leven('xabxcdxxefxgx', 'abcdefg'), 6);
-	t.is(leven('javawasneat', 'scalaisgreat'), 7);
-	t.is(leven('example', 'samples'), 3);
-	t.is(leven('sturgeon', 'urgently'), 6);
-	t.is(leven('levenshtein', 'frankenstein'), 6);
-	t.is(leven('distance', 'difference'), 5);
-	t.is(leven('因為我是中國人所以我會說中文', '因為我是英國人所以我會說英文'), 2);
-	t.end();
+test(t => {
+	t.is(m('a', 'b'), 1);
+	t.is(m('ab', 'ac'), 1);
+	t.is(m('ac', 'bc'), 1);
+	t.is(m('abc', 'axc'), 1);
+	t.is(m('kitten', 'sitting'), 3);
+	t.is(m('xabxcdxxefxgx', '1ab2cd34ef5g6'), 6);
+	t.is(m('cat', 'cow'), 2);
+	t.is(m('xabxcdxxefxgx', 'abcdefg'), 6);
+	t.is(m('javawasneat', 'scalaisgreat'), 7);
+	t.is(m('example', 'samples'), 3);
+	t.is(m('sturgeon', 'urgently'), 6);
+	t.is(m('levenshtein', 'frankenstein'), 6);
+	t.is(m('distance', 'difference'), 5);
+	t.is(m('因為我是中國人所以我會說中文', '因為我是英國人所以我會說英文'), 2);
 });
